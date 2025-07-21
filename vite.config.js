cab094ef62f01eb265cdbaf6be5799427658bbd2
Novsochetra <sochetra12.nov@gmail.com>
2025-07-21 16:56:10 +0700

    chore: adding obfuscator for client side code

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteJsObfuscator from "@sochetra-nov/vite-plugin-js-obfuscator";
+
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteJsObfuscator({
+      compact: true,
+      controlFlowFlattening: true,
+      debugProtection: false,
+      debugProtectionInterval: 1000,
+      selfDefending: true,
+    }),
+  ],
   base: "/public",
   build: {
     ssr: false,
